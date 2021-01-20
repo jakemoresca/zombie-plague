858b6d80181e8da0b0d3129062ac823d516ec68f
Jake Daryl Moresca <jmoresca@outlook.com>
2021-01-20 22:11:58 +0100

    select indicator

---
+++ Assets/UI/InteractionButtons/SelectIndicator.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public class SelectIndicator : Node2D
+{
+	// Declare member variables here. Examples:
+	private Map _map;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_map = this.GetNode<Map>("../../MainMap");
+
+		_map.Connect("FinishedUpdating", this, "_on_Map_finished_updating");
+	}
+
+	private void _on_Map_finished_updating()
+	{
+		var tileSize = _map.GetTileSize();
+		var initCoordinates = _map.GetInitCoordinates();
+		var currentSelectedNode = _map.GetSelectedNode();
+
+		if (currentSelectedNode is Player player)
+		{
+			var gridPosition = player.GetGridPosition();
+			var position = new GridPosition { Row = gridPosition.Row, Column = gridPosition.Column };
+
+			initCoordinates.Item2 += 28;
+
+			this.Position = GridHelper.GetTargetPosition(position, tileSize, initCoordinates);
+		}
+	}
+}
